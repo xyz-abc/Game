4989cd5e3a94ff7d413b90c9813156c181c935a6
xyzabc <julian.hawel@gmail.com>
2017-04-29 04:05:25 +0200

    7

---
+++ Game/InputHandler/InputHandler.cs
@@ -0,0 +1,19 @@
+﻿using EngineWrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.
+namespace InputHandler
+{
+    public class InputHandler
+    {
+        EngineWrapperClass eng;
+        InputHandler(EngineWrapperClass e)
+        {
+            KeyBoardState
+            eng = e;
+        }
+    }
+}
